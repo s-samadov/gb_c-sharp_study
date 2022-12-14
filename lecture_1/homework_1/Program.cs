7ab49d3546815613b3b43c79c8c574385b61a0f9
s.samadov <sz.samadov@gmail.com>
2022-12-14 17:32:47 +0500

    add homework_1 "task with dog"

---
+++ lecture_1/homework_1/Program.cs
@@ -0,0 +1,26 @@
+﻿int distance = 10000;
+int first_fried_speed = 1;
+int second_friend_speed = 2;
+int dog_speed = 5;
+int friend = 2;
+int count = 0;
+int time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (first_fried_speed + dog_speed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (second_friend_speed + dog_speed);
+        friend = 1;
+    }
+    distance = distance - (first_fried_speed + second_friend_speed) * time;
+    count = count + 1;
+}
+Console.Write("Dog run ");
+Console.Write(count);
+Console.Write(" times");
