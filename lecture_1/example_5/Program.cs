0516326674766087440db6a0c12f462749bb3da5
s.samadov <sz.samadov@gmail.com>
2022-12-14 15:43:27 +0500

    add example_5 "if - else"

---
+++ lecture_1/example_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter the name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "kira")
+{
+    Console.WriteLine("OMG, this is Kira");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
