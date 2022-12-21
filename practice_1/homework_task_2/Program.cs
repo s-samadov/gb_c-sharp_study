a70c90f53793d2a2ba6985e467a976f7cc2badfa
s.samadov <sz.samadov@gmail.com>
2022-12-21 15:05:39 +0500

    add homework_task_2 "max number"

---
+++ practice_1/homework_task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+int a;
+int b;
+int max_number;
+
+Console.Write("Enter number A: ");
+a = int.Parse(Console.ReadLine());
+Console.Write("Enter number B: ");
+b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    max_number = a;
+}
+else
+{
+    max_number = b;
+}
+Console.Write("Max number: ");
+Console.Write(max_number);
+
+
+
