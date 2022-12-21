4ac6b743042b42a1fab20aa66bee648af5872abf
s.samadov <sz.samadov@gmail.com>
2022-12-21 16:36:07 +0500

    add homework_task_5 "all honest number before N"

---
+++ practice_1/homework_task_5/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int N;
+int elem = 1;
+
+Console.Write("Enter number: ");
+N = int.Parse(Console.ReadLine());
+
+for (elem = 1; elem < N; elem++)
+{
+    if(elem % 2 == 0)
+    {
+        Console.WriteLine(elem);
+    }
+}
