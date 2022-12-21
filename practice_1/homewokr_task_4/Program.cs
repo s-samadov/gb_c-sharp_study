80c6ee709a88130523a064e55251a4320753c219
s.samadov <sz.samadov@gmail.com>
2022-12-21 15:47:10 +0500

    add homewokr_task_4 "honest or odd"

---
+++ practice_1/homewokr_task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int numb;
+
+Console.Write("Enter number: ");
+numb = int.Parse(Console.ReadLine());
+
+if (numb % 2 == 0)
+{
+    Console.Write("honest");
+}
+else
+{
+    Console.Write("odd");
+}
