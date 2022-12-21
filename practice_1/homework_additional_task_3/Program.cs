3f57bd136fe9390d5f389d291851cd6dc41a3c5c
s.samadov <sz.samadov@gmail.com>
2022-12-21 18:08:47 +0500

    homework_additional_task_3 "-n to +n"

---
+++ practice_1/homework_additional_task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int N;
+int elem;
+
+Console.Write("Enter number: ");
+N = int.Parse(Console.ReadLine());
+
+for (elem = N * (-1); elem <= N; elem++)
+{
+    if(elem % 2 == 0)
+    {
+        Console.WriteLine(elem);
+    }
+}
