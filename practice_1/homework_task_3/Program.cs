c79b90d6e2186f0651ffa2475287fc8e55873f90
s.samadov <sz.samadov@gmail.com>
2022-12-21 15:36:08 +0500

    add homework_task_3 "max of 3 numbers"

---
+++ practice_1/homework_task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int a;
+int b;
+int c;
+int max_number = 0;
+
+Console.Write("Enter number A: ");
+a = int.Parse(Console.ReadLine());
+Console.Write("Enter number B: ");
+b = int.Parse(Console.ReadLine());
+Console.Write("Enter number C: ");
+c = int.Parse(Console.ReadLine());
+
+if (a>max_number) max_number = a;
+if (b>max_number) max_number = b;
+if (c>max_number) max_number = c;
+
+Console.Write("Max number = ");
+Console.WriteLine(max_number);
