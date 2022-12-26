730ecf2625ec7e3492a7d6f7584df765a123d18b
s.samadov <sz.samadov@gmail.com>
2022-12-26 13:10:38 +0500

    add task_2 "del central X from XXX"

---
+++ practice_2/task_2/Program.cs
@@ -0,0 +1,7 @@
+﻿int numb = new Random().Next(100, 999);
+int res1 = numb / 100;
+int res2 = numb % 10;
+Console.WriteLine(res1);
+Console.WriteLine(res2);
+int res3 = res1 * 10 + res2;
+Console.WriteLine(res3);
