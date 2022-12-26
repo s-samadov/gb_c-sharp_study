29a5edb824fe6f361a6cc596acfb4df74ae0044f
s.samadov <sz.samadov@gmail.com>
2022-12-26 12:55:35 +0500

    add task_1 "which number higher"

---
+++ practice_2/task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int result;
+int result_2;
+int numb = new Random().Next(10, 99);
+result = numb % 10;
+result_2 = numb / 10;
+
+Console.WriteLine(numb);
+if (result_2 > result)
+{
+    Console.WriteLine(result_2);
+}
+else
+{
+Console.WriteLine(result);
+}
