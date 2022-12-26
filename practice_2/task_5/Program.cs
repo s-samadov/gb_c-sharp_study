b3e0ef71ac99c55af0b66f04409ca45e30ca52fa
s.samadov <sz.samadov@gmail.com>
2022-12-26 13:41:30 +0500

    add task_5 "two degree conditions"

---
+++ practice_2/task_5/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int numb_A;
+int numb_B;
+
+Console.Write("Enter number A: ");
+numb_A = int.Parse(Console.ReadLine());
+Console.Write("Enter number B: ");
+numb_B = int.Parse(Console.ReadLine());
+
+if (numb_B * numb_B == numb_A | numb_A * numb_A == numb_B)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
