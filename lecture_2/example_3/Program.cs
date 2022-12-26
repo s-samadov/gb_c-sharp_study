a0a10d44b978c7db30e17efb2fcfc75ecb8776d8
s.samadov <sz.samadov@gmail.com>
2022-12-26 12:08:20 +0500

    add example_3 "search number"

---
+++ lecture_2/example_3/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array_2 = {1, 102, 14, 19, 2, 102, 60, 8};
+int n = array_2.Length;
+int find = 102;
+int index = 0;
+while (index < n)
+{
+    if(array_2[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1; // index++
+}
