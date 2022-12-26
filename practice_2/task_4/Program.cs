8487db872586b8b24d169081b6563e0615e130e7
s.samadov <sz.samadov@gmail.com>
2022-12-26 13:36:47 +0500

    "task_4 "two multiplicity conditions"

---
+++ practice_2/task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+int numb;
+Console.Write("Enter number: ");
+numb = int.Parse(Console.ReadLine());
+if (numb % 7 == 0 & numb % 23 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
