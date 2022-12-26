e04319006417486ab5a12f0fdaf8f8d93eaa29b5
s.samadov <sz.samadov@gmail.com>
2022-12-26 13:29:12 +0500

    add task_3 "multiple or no multiple"

---
+++ practice_2/task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+int numb_a = new Random().Next(0, 99);
+Console.Write("Number A: ");
+Console.WriteLine(numb_a);
+int numb_b = new Random().Next(0, 99);
+Console.Write("Number B: ");
+Console.WriteLine(numb_b);
+
+if (numb_a % numb_b == 0)
+{
+    Console.WriteLine("multiple");
+}
+else
+{
+int res = numb_a % numb_b;
+Console.Write("no multiple, remainder is ");
+Console.WriteLine(res);
+}
+
