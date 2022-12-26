6d09daf9abdaac913c09375a7da9f388d3589aa9
s.samadov <sz.samadov@gmail.com>
2022-12-26 15:01:40 +0500

    add homework_task_3 "number for days"

---
+++ practice_2/homework_task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Enter day number: ");
+int day_numb = Convert.ToInt32(Console.ReadLine());
+if (day_numb == 6 | day_numb == 7)
+{
+    Console.Write("This is day off");
+}
+else
+{
+    if (day_numb > 7 | day_numb < 1)
+    {
+        Console.Write("You enter incorrect number");
+    }
+    else
+    {
+        Console.Write("This is working day");
+    }
+}
