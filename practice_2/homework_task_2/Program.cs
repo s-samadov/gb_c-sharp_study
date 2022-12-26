ebb48b686e23c2b816a94abbd8dbfd36d85225aa
s.samadov <sz.samadov@gmail.com>
2022-12-26 14:37:15 +0500

    add homework_task_2 "third number in anynumber"

---
+++ practice_2/homework_task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int numb = -32; //new Random().Next(100, 999);
+if (numb > 100 | numb < -100)
+{
+    while (numb > 999 | numb < -999)
+	{
+		numb = numb / 10;
+	}
+	int result = numb % 10;
+    if (result < 0)
+    {
+        result = result * -1;
+    }
+    Console.WriteLine($"{numb}, {result}");
+}
+else
+{
+    Console.WriteLine("no third digit");
+}
