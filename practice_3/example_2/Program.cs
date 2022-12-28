9661864fd42cfff613414f387e6f6cf71af4eeb6
s.samadov <sz.samadov@gmail.com>
2022-12-28 20:44:34 +0500

    example_2 "XY coordinate ver2"

---
+++ practice_3/example_2/Program.cs
@@ -0,0 +1,33 @@
+﻿void print_coordinate(int numb_x, int numb_y)
+{
+if (numb_x > 0 & numb_y > 0)
+{
+    Console.WriteLine("1 quater");
+}
+
+else if (numb_x > 0 & numb_y < 0)
+{
+    Console.WriteLine("4 quater");
+}
+
+else if (numb_x < 0 & numb_y > 0)
+{
+    Console.WriteLine("2 quater");
+}
+
+else if (numb_x < 0 & numb_y < 0)
+{
+    Console.WriteLine("3 quater");
+}
+else
+{
+    Console.WriteLine("Incorrect combination");
+}
+}
+
+Console.Clear();
+Console.Write("Enter X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+print_coordinate(x, y);
