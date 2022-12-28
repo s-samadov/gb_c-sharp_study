5b2dc3513987656a59aaf4c612483acbaa09463d
s.samadov <sz.samadov@gmail.com>
2022-12-28 22:20:10 +0500

    add homework_tast_1 "this number is a palindrome"

---
+++ practice_3/homework_tast_1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Enter XXXXX number: ");
+string number = Console.ReadLine();
+int n = number.Length;
+
+if(n > 4 & n < 6)
+{
+    if(number[0] == number[4] & number[1] == number[3])
+    {
+        Console.WriteLine("This number is a palindrome");
+    }
+    else
+    {
+        Console.WriteLine("This number is not a palindrome");
+    }
+}
+else
+{
+    Console.WriteLine("Incorrect number");
+}
