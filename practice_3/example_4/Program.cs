9c071d71e55a145a9a7c7b51d82a09f03d6a1c52
s.samadov <sz.samadov@gmail.com>
2022-12-28 21:24:17 +0500

    add example_4 "distance between two points"

---
+++ practice_3/example_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Enter A1: ");
+int A1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter B1: ");
+int B1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter A2: ");
+int A2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter B2: ");
+int B2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow((A1 - B1), 2) + Math.Pow((A2- B2), 2));
+Console.WriteLine(result);
+
