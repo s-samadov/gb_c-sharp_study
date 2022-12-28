a02da56078e64b8ccb5a75e447c594494d6dd992
s.samadov <sz.samadov@gmail.com>
2022-12-28 21:02:46 +0500

    add example_3 "from quater to X,Y"

---
+++ practice_3/example_3/Program.cs
@@ -0,0 +1,28 @@
+﻿void print_xy(int quater)
+{
+    if(quater == 1)
+    {
+        Console.WriteLine("x>0 & y>0");
+    }
+    else if(quater == 2)
+    {
+        Console.WriteLine("x<0 & y>0");
+    }
+    else if(quater == 3)
+    {
+        Console.WriteLine("x<0 & y<0");
+    }
+    else if(quater == 4)
+    {
+        Console.WriteLine("x>0 & y<0");
+    }
+    else
+    {
+        Console.WriteLine("Incorrect quater number");
+    }
+}
+
+Console.Clear();
+Console.Write("Enter quater number: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+print_xy(numb);
