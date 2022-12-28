ab8f4839800b1ba27cc763f88e3773c3d5e97315
s.samadov <sz.samadov@gmail.com>
2022-12-28 21:31:35 +0500

    add example_5 "squat for N number"

---
+++ practice_3/example_5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int i = 1;
+Console.Write("Enter number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+while (i <= N)
+{
+    Console.WriteLine(Math.Pow(i, 2));
+    i ++;   
+}
