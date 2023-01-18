b205cc927b418589600e64bc183df1ab68032acb
s.samadov <sz.samadov@gmail.com>
2023-01-18 11:50:52 +0500

    add example_1 " 4 methods"

---
+++ lecture_3/example_1/Program.cs
@@ -0,0 +1,45 @@
+﻿ // var 1 don't return
+ void Method1()
+ {
+    Console.WriteLine("Autor is S.Samadov");
+ }
+
+Method1();
+
+ // var 2 don't return
+void Method2(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+
+Method2(msg: "Message text", count: 5);
+
+// var 3 return 
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+// var 4 return
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(4, "qwerty");
+Console.WriteLine(res);
