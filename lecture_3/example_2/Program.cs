845fb02de0627558df66e3a51697fc7ac5afd4aa
s.samadov <sz.samadov@gmail.com>
2023-01-18 12:02:03 +0500

    add example_2 "loops FOR"

---
+++ lecture_3/example_2/Program.cs
@@ -0,0 +1,22 @@
+﻿string Method5(int count, string text)
+{
+    
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method5(4, "qwerty");
+Console.WriteLine(res);
+
+for(int i = 2; i <= 10; i ++)
+{
+    for(int j = 2; j <= 10; j ++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
