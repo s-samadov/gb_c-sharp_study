6849298155ecb8203d0944399722e295e4f4b05b
s.samadov <sz.samadov@gmail.com>
2023-01-18 13:26:22 +0500

    add homework_1 "sort method max - min"

--- lecture_3/homework_1/Program.cs
+++ lecture_3/homework_1/Program.cs
@@ -1,4 +1,4 @@
-﻿//method sort min - max
+﻿//method sort max - min
 
 int [] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
 
@@ -16,14 +16,14 @@ void selection_sort(int[] array)
 {
     for (int i = 0; i < array.Length - 1; i++)
     {
-        int min_position = i;
+        int max_position = i;
         for (int j = i + 1; j < array.Length; j++)
         {
-            if (array[j] < array[min_position]) min_position = j;
+            if (array[j] > array[max_position]) max_position = j;
         }
         int temporary = array[i];
-        array[i] = array [min_position];
-        array[min_position] = temporary;
+        array[i] = array [max_position];
+        array[max_position] = temporary;
     }
 }
 
