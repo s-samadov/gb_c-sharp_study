1271cab1dfee1ecae6f18fb037295f90bf1d559a
s.samadov <sz.samadov@gmail.com>
2023-01-18 12:51:40 +0500

    add example_4 "method algorithm min - max"

---
+++ lecture_3/example_4/Program.cs
@@ -0,0 +1,32 @@
+﻿//method sort min - max
+
+int [] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void print_array(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void selection_sort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int min_position = i;
+        for (int j = i + 1; j < array.Length - 1; j++)
+        {
+            if (array[j] < array[min_position]) min_position = j;
+        }
+        int temporary = array[i];
+        array[i] = array [min_position];
+        array[min_position] = temporary;
+    }
+}
+
+print_array(arr);
+selection_sort(arr);
+print_array(arr);
