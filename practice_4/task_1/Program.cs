9e432666f4e82eb5306d07bc6bf32ef9660578b9
s.samadov <sz.samadov@gmail.com>
2023-01-19 18:38:07 +0500

    add task_1 "sum all number before N"

---
+++ practice_4/task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int sum_numb(int number)
+{
+    int i = 0;
+    int result = 0;
+    while (i <= number)
+    {
+        result = result + i;
+        i++; 
+    }
+    return result;
+}
+int sum = sum_numb(2);
+Console.WriteLine(sum);
