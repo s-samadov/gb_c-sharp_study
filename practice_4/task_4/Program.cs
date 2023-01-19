367f2c3821f3991015c198fec4f507fb6edc5ed0
s.samadov <sz.samadov@gmail.com>
2023-01-19 21:42:11 +0500

    add task_4 "create binary array"

---
+++ practice_4/task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+int[] array = new int[8];
+int i = 0;
+while (i < array.Length)
+{
+     array[i] = new Random().Next(0, 2);
+     Console.Write($"{array[i]} ");
+     i++;
+}
+*/
+
+int[] generate_array(int array_size)
+{
+    int[] result = new int[array_size];
+    for (int i = 0; i < array_size; i++)
+    {
+        result[i] = new Random().Next(2);
+    }
+    return result;
+}
+int[] array = generate_array(8);
+Console.WriteLine($" [ {String.Join(", ", array)} ] ");
+
+
