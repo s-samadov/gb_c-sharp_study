84b56588d87d9e47bcd7388b818699b99f133de4
s.samadov <sz.samadov@gmail.com>
2023-01-20 03:21:31 +0500

    add homework_task_2 "sum all numb on odd place"

---
+++ practice_5/homework_task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+int[] create_random_array(int size, int min_number, int max_number)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min_number, max_number);
+    }
+    return result;
+}
+
+void show_array(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = create_random_array(5, -100, 100);
+show_array(array);
+
+int sum = 0;
+for (int i = 1; i < array.Length; i = i + 2)
+{
+    sum = sum + array[i];
+}
+Console.WriteLine($"\nresult is: {sum}");
