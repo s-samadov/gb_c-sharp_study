ea9c10031c47afd317c0fa5995b48224a510f027
s.samadov <sz.samadov@gmail.com>
2023-01-19 18:56:37 +0500

    add task_2 "input number = output count of number"

---
+++ practice_4/task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int numb_cal (int number)
+{
+    int result = 0;
+    for (int i = 0; number > 0; i++)
+    {
+        number = number / 10;
+        if (number >= 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+int check = numb_cal(456);
+Console.WriteLine(check);
