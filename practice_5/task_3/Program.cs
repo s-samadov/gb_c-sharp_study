817c24fb05bf9f67c9edf449dbad45d2b973a15b
s.samadov <sz.samadov@gmail.com>
2023-01-20 01:02:47 +0500

    add task_3 "find number in array"

---
+++ practice_5/task_3/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+int[] create_random_array(int size, int min_number, int max_number)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min_number, max_number);
+    }
+    return result;
+}
+
+void show_array(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = create_random_array(4, 0, 6);
+show_array(array);
+
+int number = 5;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == number)
+    {
+        Console.WriteLine(true);
+        break;
+    }
+}
