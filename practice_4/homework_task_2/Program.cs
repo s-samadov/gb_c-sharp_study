ebebb7c985aaa104b36fb4a7f63be6ced2fd02e5
s.samadov <sz.samadov@gmail.com>
2023-01-19 22:38:35 +0500

    add homework_task_2 "sum number in number"

---
+++ practice_4/homework_task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int numb_cal (int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result = result + number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+int check = numb_cal(452);
+Console.WriteLine(check);
