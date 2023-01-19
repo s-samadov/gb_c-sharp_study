f77058c403356c52a31899b8af5636ec39376c02
s.samadov <sz.samadov@gmail.com>
2023-01-20 03:07:15 +0500

    add homework_task_1 "find count even numbers"

---
+++ practice_5/homework_task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+int[] create_random_array(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(100, 999);
+    }
+    return result;
+}
+
+void show_array(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = create_random_array(4);
+show_array(array);
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"\nresult is: {count}");
