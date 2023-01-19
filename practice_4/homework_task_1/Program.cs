dba59cdb1bf43d104aae9a5aabd0c18ecfe1ba69
s.samadov <sz.samadov@gmail.com>
2023-01-19 22:18:59 +0500

    add homework_task_1 "exponentiation a^B"

---
+++ practice_4/homework_task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+int mult_numb(int numb_a, int numb_b)
+{
+    int result = 1;
+    for (int i = 0; i < numb_b; i++)
+    {
+        result = result * numb_a;
+    }
+    return result;
+}
+int end = mult_numb(2, 4);
+Console.WriteLine(end);
