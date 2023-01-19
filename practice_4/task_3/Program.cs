1c1bf37a292028bbe44e1cf06ae6ae0193734d42
s.samadov <sz.samadov@gmail.com>
2023-01-19 19:00:43 +0500

    add task_3 "multiple numbers until N"

---
+++ practice_4/task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int sum_numb(int number)
+{
+    int i = 1;
+    int result = 1;
+    while (i <= number)
+    {
+        result = result * i;
+        i++; 
+    }
+    return result;
+}
+int sum = sum_numb(5);
+Console.WriteLine(sum);
