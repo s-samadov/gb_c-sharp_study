a66a31e44e68af8dce90cb073a92ca18762b91ce
s.samadov <sz.samadov@gmail.com>
2023-01-23 11:05:33 +0500

    add example_3 "recursion - factorial"

---
+++ lecture_4/example_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+double Factorial(int n)
+{
+    if (n == 1) return 1; // 1! = 1; 0! = 1
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 1; i < 40; i ++)
+{
+Console.WriteLine($"{i}! = {Factorial(i)}");
+}
