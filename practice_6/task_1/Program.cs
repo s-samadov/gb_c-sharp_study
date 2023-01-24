7a7e740d94fc22f97573c3ae64d7ad3b9bb767ce
s.samadov <sz.samadov@gmail.com>
2023-01-24 10:17:41 +0500

    add task_1 "2 ways to reverse array"

---
+++ practice_6/task_1/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.Clear();
+
+int[] CreateRandomArray(int size, int min_number, int max_number)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min_number, max_number);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] array = CreateRandomArray(4, 1, 10);
+PrintArray(array);
+
+/*
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+Console.WriteLine();
+ReverseArray(array);
+PrintArray(array);
+*/
+
+
+int[] newArray=new int[array.Length];
+
+void NewArray(int[] array)
+{
+   for(int i=0; i<array.Length; i++) newArray[i]=array[array.Length - 1 - i];
+}
+
+Console.WriteLine();
+NewArray(array);
+PrintArray(newArray);
+
