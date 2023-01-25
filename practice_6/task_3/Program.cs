8fe0efff6352cc4c0beaa40346878001bf9301a8
s.samadov <sz.samadov@gmail.com>
2023-01-25 11:17:12 +0500

    add task_3 "from decimal to binary" no fully ready

---
+++ practice_6/task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+while (number > 0)
+{
+    Console.Write(number % 2);
+    number = number / 2; 
+}
+
