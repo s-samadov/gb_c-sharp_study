8ea3f72ab1a34074ae9e57acda6abca16255e242
s.samadov <sz.samadov@gmail.com>
2023-01-26 09:06:14 +0500

    add task_4 "Fibonacci numbers"

---
+++ practice_6/task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 1; i < number; i = i + result)
+{
+    Console.Write($"{i} ");
+    result=i-result;
+}
+ 
+ 
