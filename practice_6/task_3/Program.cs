f2d2a95bd863f68214d3737d79e711e78af155b6
s.samadov <sz.samadov@gmail.com>
2023-01-26 10:33:46 +0500

    add task_3 "from decimal to binary"

--- practice_6/task_3/Program.cs
+++ practice_6/task_3/Program.cs
@@ -1,10 +1,25 @@
-﻿Console.Clear();
+﻿
+
+Console.Clear();
 Console.Write("Enter number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int res = 0;
+int[] array = new int[Convert.ToInt32(Math.Log2(number)) + 1];
+int i = 0;
 while (number > 0)
 {
-    Console.Write(number % 2);
+    Console.Write($"{number % 2} ");
+    array[array.Length - i - 1] = number % 2;
+    i++;
     number = number / 2; 
 }
 
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+ShowArray(array);
