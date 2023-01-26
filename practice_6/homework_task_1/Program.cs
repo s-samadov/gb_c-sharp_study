45febbed577d65753c50d2b037f6c45351e9679a
s.samadov <sz.samadov@gmail.com>
2023-01-26 11:22:55 +0500

    add homework_task_1 "count number greater than 0"

---
+++ practice_6/homework_task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.WriteLine("Enter various numbers: ");
+Console.WriteLine("Enter number 999 for stop");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int j = 0;
+while (true)
+{
+    if (number == 999) // for stop
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Enter another number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0)
+        {
+            j++;
+        }
+    }
+    i++;
+}
+Console.WriteLine($"Number greater than 0: {j}");
