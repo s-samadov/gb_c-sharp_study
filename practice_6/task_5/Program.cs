dbc0ec4e84779628ed47abdcdbbc3d5a07e8469d
s.samadov <sz.samadov@gmail.com>
2023-01-26 10:56:18 +0500

    add task_5 "copy array by each element"

---
+++ practice_6/task_5/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+Console.Clear();
+
+int[] CreateRandomArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(100, 999);
+    }
+    return result;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int [] CopyArray(int[] array)
+{
+    int [] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[i];
+    }
+    return result;
+}
+
+int[] array = CreateRandomArray(5);
+ShowArray(array);
+Console.WriteLine();
+int[] copyArray = CopyArray(array);
+ShowArray(copyArray);
