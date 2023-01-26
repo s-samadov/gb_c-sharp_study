742c9d0e705ba8d83206298bf4f05403f28156b5
s.samadov <sz.samadov@gmail.com>
2023-01-26 10:45:02 +0500

    add task_4 "Fibonacci number"

--- practice_6/task_4/Program.cs
+++ practice_6/task_4/Program.cs
@@ -1,9 +1,21 @@
 ﻿Console.Clear();
 Console.Write("Enter number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int result = 1;
-for (int i = 1; i < number; i = i + result)
+void ShowArray(int[] array)
 {
-    Console.Write("{0} ", i);
-    result = i - result;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
 }
+
+int [] f = new int [number];
+f[0] = 0;
+f[1] = 1;
+for (int i = 2; i < number; i++)
+{
+    f[i] = f[i - 1] + f [i - 2];
+}
+
+ShowArray(f);
