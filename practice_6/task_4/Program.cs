3e4b4733b3ab828d959ec09a35286e972b77ab74
s.samadov <sz.samadov@gmail.com>
2023-01-26 09:11:06 +0500

    add task_4 "Fibonacci number" not correct

--- practice_6/task_4/Program.cs
+++ practice_6/task_4/Program.cs
@@ -4,8 +4,6 @@ int number = Convert.ToInt32(Console.ReadLine());
 int result = 1;
 for (int i = 1; i < number; i = i + result)
 {
-    Console.Write($"{i} ");
-    result=i-result;
+    Console.Write("{0} ", i);
+    result = i - result;
 }
- 
- 
