0742c66892dc85e2ab0f52d77e5daa147add27a1
s.samadov <sz.samadov@gmail.com>
2023-01-27 11:21:10 +0500

    add task_2 "2 layer array Amn = m + n"

---
+++ practice_7/task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+Console.Write("Enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateArray(int r, int c)
+{
+    int[,] result = new int[r, c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = CreateArray(rows, columns);
+PrintArray(array);
