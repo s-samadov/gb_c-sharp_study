6681d66e8aad2dcca0e9ac163845abfa36ccba58
s.samadov <sz.samadov@gmail.com>
2023-01-27 11:01:39 +0500

    add task_1 "2 layer array"

---
+++ practice_7/task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+Console.Write("Enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateArray(int r, int c, int minValue, int maxValue)
+{
+    int[,] result = new int[r, c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = CreateArray(rows, columns, 0, 10);
+PrintArray(array);
