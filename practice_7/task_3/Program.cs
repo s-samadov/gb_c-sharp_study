6b6be9b12135a5e5fc6f45f8c31832e5ac70a203
s.samadov <sz.samadov@gmail.com>
2023-01-30 10:14:10 +0500

    add task_3 "2 layer array with even^2"

---
+++ practice_7/task_3/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.Clear();
+Console.Write("Enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] Creat2LayerArray(int r, int c)
+{
+    int[,] result = new int[r, c];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return result;
+}
+
+void Show2LayerArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = Creat2LayerArray(rows, columns);
+Show2LayerArray(array);
+
+void TestArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = Convert.ToInt32(Math.Pow(array[i, j], 2));
+            }
+        Console.WriteLine();
+        }
+    }
+}
+TestArray(array);
+Show2LayerArray(array);
+
