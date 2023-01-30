48a17328733e4160f597ca5c9a10116ddd3e392e
s.samadov <sz.samadov@gmail.com>
2023-01-30 12:05:37 +0500

    add homework_task_1 "2layer array with float numb"

---
+++ practice_7/homework_task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+Console.Write("Enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] CreateArray(int r, int c)
+{
+    double[,] result = new double[r, c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            result[i, j] = Math.Round((new Random().NextDouble() * 200 - 100), 3);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+          Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array = CreateArray(rows, columns);
+PrintArray(array);
