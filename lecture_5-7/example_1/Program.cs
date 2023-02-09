cb16f20bf859bd2bf35c546c57119bc1829d5c86
s.samadov <sz.samadov@gmail.com>
2023-02-09 22:42:12 +0500

    add example_1 "Show catalog"

---
+++ lecture_5-7/example_1/Program.cs
@@ -0,0 +1,8 @@
+﻿string path = "C:/Users/s.samadov/Desktop/gb_c-sharp_study/lecture_1/example_1";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
