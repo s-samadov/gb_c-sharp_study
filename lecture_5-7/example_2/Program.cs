312ab973f27b4148a37738c0bc1c2b7c66e4da3b
s.samadov <sz.samadov@gmail.com>
2023-02-09 23:01:57 +0500

    add example_2 "Recursion for show files"

---
+++ lecture_5-7/example_2/Program.cs
@@ -0,0 +1,18 @@
+﻿void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + "  ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+string path = "C:/Users/s.samadov/Desktop/gb_c-sharp_study/lecture_1/example_1";
+CatalogInfo(path);
