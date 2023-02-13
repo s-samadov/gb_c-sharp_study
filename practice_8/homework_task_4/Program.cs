aa6268fa1078601b59ad0b131776acc9b12441cd
s.samadov <sz.samadov@gmail.com>
2023-02-13 21:53:26 +0500

    add homework_task_4 "3layer array"

---
+++ practice_8/homework_task_4/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+Console.Write("Enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number of heights: ");
+int heights = Convert.ToInt32(Console.ReadLine());
+
+
+int[,,] CreateArray(int r, int c, int h)
+{
+    int[,,] result = new int[r, c, h];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            for (int k = 0; k < h; k++)
+            {
+                result[i, j, k] = new Random().Next(10, 99);
+            }
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            for(int k = 0; k < matr.GetLength(2); k++)
+            {
+                Console.Write($"{matr[i, j, k]} ({i},{j},{k}) ");
+            }
+            Console.WriteLine();        
+        }
+    }
+}
+
+int[,,] array = CreateArray(rows, columns, heights);
+PrintArray(array);
