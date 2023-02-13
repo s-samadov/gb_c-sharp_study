86be75898e085f8e246301b5d8efbd2cefc8577e
s.samadov <sz.samadov@gmail.com>
2023-02-13 19:50:32 +0500

    add task_2 "replacement r to c"

---
+++ practice_8/task_2/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.Clear();
+Console.Write("Enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateArray(int r, int c)
+{
+    int[,] result = new int[r, c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            result[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = CreateArray(rows, columns);
+PrintArray(array);
+
+int[,] ReversArray(int[,] matr)
+{
+    int[,] result = new int[matr.GetLength(0), matr.GetLength(1)];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            result[j, i] = matr[i, j];
+        }
+    }
+    return result;
+}
+
+Console.WriteLine();
+if (rows == columns)
+{
+int[,] NewArray = ReversArray(array);
+PrintArray(NewArray);
+}
+else Console.Write("Replacement is not possible, enter correct value");
