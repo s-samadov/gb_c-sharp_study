7df281f45abd80a845a3446a60880cca53b6c5b5
s.samadov <sz.samadov@gmail.com>
2023-02-16 17:46:11 +0500

    add Final_task_c# "enter array from keybord <= 3"

---
+++ Final_task_c#/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Enter count of elemets in array: ");
+int elCount = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[elCount];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Enter element of array: ");
+    array[i] = Console.ReadLine();
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.Write("Array elements whose length <= 3 characters long: ");
+PrintArray(array);
