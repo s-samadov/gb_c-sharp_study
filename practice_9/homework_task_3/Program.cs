4d8a576dfb311122562a8eda2fe08532898d8ea1
s.samadov <sz.samadov@gmail.com>
2023-02-20 19:13:35 +0500

    add homework_task_3 "Function Akkerman"

---
+++ practice_9/homework_task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Enter the number M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number N: ");
+int N= Convert.ToInt32(Console.ReadLine());
+
+int Function(int M, int N)
+{
+    if (M == 0)
+        return N + 1;
+    else
+    if ((M != 0) && (N == 0))
+        return Function(M - 1, 1);
+    else
+        return Function(M - 1, Function(M, N - 1));
+}
+Console.Write(Function(M, N));
