44b606ecc91673e7985e8c544cd7cba9b547013b
s.samadov <sz.samadov@gmail.com>
2023-02-20 18:58:55 +0500

    add homework_task_2 "sum of numbers from m to n"

---
+++ practice_9/homework_task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Enter the number M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number N: ");
+int N= Convert.ToInt32(Console.ReadLine());
+
+
+
+int Function(int M, int N, int sum)
+{
+    if (M <= N)
+    {
+        sum  = sum + M;
+        M++;
+        return Function(M, N, sum);
+    }
+    return sum;  
+}
+Console.WriteLine($"Sum of the numbers is: {Function(M, N, 0)}");
