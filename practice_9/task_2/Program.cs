eb1fd64b5a0ed74433a54739c8179ff3dc19b42c
s.samadov <sz.samadov@gmail.com>
2023-02-20 15:54:14 +0500

    add task_2 "all natural number from M to N"

---
+++ practice_9/task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Enter the number M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Function(M, N);
+void Function(int M, int N)
+{
+    Console.Write($"{M} ");
+    if (M == N) return;
+    Function(M + 1, N);
+}
