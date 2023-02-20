5bddf72f49a70d9a4c6c2f458fa4366155bc280b
s.samadov <sz.samadov@gmail.com>
2023-02-20 17:25:35 +0500

    add homework_task_1 "all number from n to 1"

---
+++ practice_9/homework_task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Enter the number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Function(n);
+void Function(int n)
+{
+    Console.Write($"{n} ");
+    if (n == 1) return;
+    Function(n - 1);
+}
