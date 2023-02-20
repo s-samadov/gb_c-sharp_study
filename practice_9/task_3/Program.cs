317ca994b4ee713797285683346a0509d07f25e2
s.samadov <sz.samadov@gmail.com>
2023-02-20 16:51:49 +0500

    add task_3 "sum of number of number M"

---
+++ practice_9/task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Enter the number M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+
+
+int Function(int M)
+{
+    int sum = 0;   
+    if (M == 0) return 0;
+    sum  = M % 10 + Function(M / 10);
+    return sum;    
+}
+Console.WriteLine(Function(M));
